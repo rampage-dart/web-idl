c50df82162933de5bf2ab860f41e7c4bef7089f1
Don <don.j.olmstead@gmail.com>
2020-08-03 12:08:07 -0700

    feat(): Add ArgumentElement interface

--- lib/src/element/element.dart
+++ lib/src/element/element.dart
@@ -3,6 +3,8 @@
 // Use of this source code is governed by a zlib license that can be found in
 // the LICENSE file.
 
+import 'type.dart';
+
 /// The base class for all of the constructs within the WebIDL element model.
 ///
 /// The element model depicts the semantics of the WebIDL format. It represents
@@ -30,4 +32,18 @@ abstract class FragmentElement implements Element {
 abstract class EnumElement implements Element {
   /// The set of valid strings for the enumeration.
   List<String> get values;
+}
+
+//------------------------------------------------------------------
+// WebIDL member elements
+//------------------------------------------------------------------
+
+/// An argument to an operation or callback.
+abstract class ArgumentElement implements Element {
+  /// The type for the argument.
+  WebIdlType get type;
+  /// Whether the argument is optional.
+  bool get isOptional;
+  /// Whether the argument is variadic.
+  bool get isVariadic;
 }
