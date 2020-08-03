eaaee3202d0a91c90d624b0de885eb258bb61940
Don <don.j.olmstead@gmail.com>
2020-08-03 12:51:20 -0700

    feat(): Add root element library

---
+++ lib/src/element.dart
@@ -0,0 +1,7 @@
+// Copyright (c) 2020 the Rampage Project Authors.
+// Please see the AUTHORS file for details. All rights reserved.
+// Use of this source code is governed by a zlib license that can be found in
+// the LICENSE file.
+
+export 'element/element.dart';
+export 'element/type.dart';
