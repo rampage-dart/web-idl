e3cea56f24e923e1e82c4506d2dca9df91961147
Don <don.j.olmstead@gmail.com>
2021-07-19 20:51:51 -0700

    feat(): Provide a simple way to parse a fragment

--- lib/web_idl.dart
+++ lib/web_idl.dart
@@ -3,6 +3,19 @@
 // Use of this source code is governed by a zlib license that can be found in
 // the LICENSE file.
 
+import 'src/element.dart';
+import 'src/parser/element_builder.dart';
+import 'src/parser/parser.dart';
+
 export 'src/element.dart';
 export 'src/type.dart';
 export 'src/types.dart';
+export 'src/visitor.dart';
+
+/// Parse the fragment defined in the [value].
+///
+/// This is a way to quickly parse a single fragment.
+FragmentElement parseFragment(String value) =>
+    _parser.parse(value).value.build();
+
+late final _parser = WebIdlParserDefinition().build<FragmentBuilder>();
